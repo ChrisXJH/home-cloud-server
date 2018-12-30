a7322645c9a8fa71e23585689e7d36bda54b63b9
Chris Xu <gzchrisxjh@gmail.com>
2018-12-29 23:23:53 -0500

    Add cross origin headers

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+});
+
 app.get('/', logRequest, (req, res) => res.send('Server is running...'));
 
 app.get('/storage', logRequest, authenticateUser, (req, res) => {
