f800c46951aad59645b0264afbf4c4bb5563f3ad
Chris Xu <gzchrisxjh@gmail.com>
2018-12-29 23:24:25 -0500

    minor fix

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.get('/', logRequest, (req, res) => res.send('Server is running...'));
